be31a33c62ed216849254482f313054ffd6edeb5
Hicran Ertuğral <hicranertugral@gmail.com>
2023-03-20 17:04:06 +0300

    dinamik map

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 function App() {
-const company = "Frontend Co."
-const description = "Lorem ipsum dolor sit amet"
-const courses = [
-{id: 1, course : "HTML asldkşalkdsşlak"},
-{id: 2, course : "CSS"},
-{id: 3, course : "JAVASCRIPT"},
-{id: 4, course : "REACT"}
+  const company = "Frontend Co.";
+  const description = "Lorem ipsum dolor sit amet";
+  const courses = [
+    { id: 1, course: "HTML asldkşalkdsşlak" },
+    { id: 2, course: "CSS" },
+    { id: 3, course: "JAVASCRIPT" },
+    { id: 4, course: "REACT" },
+  ];
 
-]
+  return (
+    <div className="App">
+      <h1>{company}</h1>
+      <p>{description}</p>
+      <h4>{`Courses (${courses.length})`}</h4>
+      {/* <li>{courses[0].course}</li>
+      <li>CSS</li>
+      <li>JAVASCRIPT</li>
+      <li>REACT</li> */}
+<ul>
+  {courses.map((e, i ) => <li key={e.id}>{e.course}</li>)}
+</ul>
 
-  return <div className="App">
-    <h1>{company}</h1>
-    <p>{description}</p>
-    <h4>{`Courses (${courses.length})`}</h4>
-    <li>{courses[0].course}</li>
-    <li>CSS</li>
-    <li>JAVASCRIPT</li>
-    <li>REACT</li>
-  </div>;
+    </div>
+  );
 }
 
 export default App;
