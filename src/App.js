8597f6efe69b2dea8f5ae8c6ea6a44dd16fa2a99
Hicran Ertuğral <hicranertugral@gmail.com>
2023-03-21 10:53:40 +0300

    Default Paramaetre

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import Heading from "./components/Heading";
 function App() {
   return (
     <div>
-    <Heading bgColor={"#666"} color={"white"} title={"Frontend Co."} />
-    <Heading bgColor={"#578"} color={"#32c"} title={"Backend Co."} />
+      <Heading bgColor={"#666"} color={"white"} />
     </div>
   );
 }
+
+Heading.defaultProps = {
+  title: "Default Heading",
+  color: "#36b",
+  bgColor: "#333",
+};
 
 export default App;
