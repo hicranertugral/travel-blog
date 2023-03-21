92fdef329084929238d2f8752c8f5f43298face7
Hicran Ertuğral <hicranertugral@gmail.com>
2023-03-21 09:27:18 +0300

    Değişken olarak rengi dinamikleştirme

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Heading = () => {
+  const styling = {
+    backgroundColor: "#333",
+    color: "white",
+  };
+
   return (
     <div>
-        <h1 style={{backgroundColor: "#333", color:"White"}}>Frontend Co.</h1>
+      <h1 style={{ backgroundColor: styling.backgroundColor, color: "White" }}>Frontend Co.</h1>
     </div>
-  )
-}
-export default Heading
+  );
+};
+export default Heading;
