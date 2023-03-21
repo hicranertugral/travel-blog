bd8bc2d0d891c84d2f5e8ec6745fe6ea73bfafbc
Hicran Ertuğral <hicranertugral@gmail.com>
2023-03-21 09:23:52 +0300

    Başlık inline stillendirme

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
-function App() {
-  const company = "Frontend Co.";
-  const description = "Lorem ipsum dolor sit amet";
-  const courses = [
-    { id: 1, course: "HTML asldkşalkdsşlak" },
-    { id: 2, course: "CSS" },
-    { id: 3, course: "JAVASCRIPT" },
-    { id: 4, course: "REACT" },
-  ];
+import Heading from "./components/Heading";
 
-  let showCourses = true;
+function App() {
   return (
     <div>
-      <h1 className="heading">{company}</h1>
-      <p>{description}</p>
-      {showCourses && (
-        <div>
-          <h4>{`Courses (${courses.length})`}</h4>
-          <ul>
-            {courses.map((e, i) => (
-              <li key={e.id}>{e.course}</li>
-            ))}
-          </ul>
-        </div>
-      )}
+    <Heading />
     </div>
   );
 }
---
+++ src/components/Heading.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Heading = () => {
+  return (
+    <div>
+        <h1 style={{backgroundColor: "#333", color:"White"}}>Frontend Co.</h1>
+    </div>
+  )
+}
+export default Heading
