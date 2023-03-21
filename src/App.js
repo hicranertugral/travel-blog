2b2f3f18645ae1a20433d76d9d1b15a22aaac16d
Hicran Ertuğral <hicranertugral@gmail.com>
2023-03-21 10:41:27 +0300

    Props kullanarak çoğaltma

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div>
     <Heading bgColor={"#666"} color={"white"} title={"Frontend Co."} />
+    <Heading bgColor={"#578"} color={"#32c"} title={"Backend Co."} />
     </div>
   );
 }
